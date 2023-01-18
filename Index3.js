488aa8949f6b3d46d48bf39174cb82546a792f9e
Krystian popiolek <114857637+Kashumy@users.noreply.github.com>
2023-01-18 18:59:19 +0100

    Add files via upload

---
+++ Index3.js
@@ -0,0 +1,13 @@
+/*if (gttsv > 20 && gttsv < 22) {
+  document.getElementById('log').innerHTML = "join my github";
+}
+onload =  function gtts() {
+	setTimeout(doSomething2, 800);
+	function doSomething2() {
+console.log('game time tick speed +1');
+gttsv += 1;
+
+          console.log(gttsv);
+	setTimeout(800);
+	return gtts();
+}}
---
+++ Staty.nedryt.md
@@ -0,0 +1,6 @@
+# md 🙂 dane
+od 10 do 100fps
+
+maksymalna odległość mapy X)^X 
+
+- 
