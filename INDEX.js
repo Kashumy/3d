060fbaeb975df95599136da7d00b4151f6a335c7
Krystian popiolek <114857637+Kashumy@users.noreply.github.com>
2023-01-23 20:00:44 +0100

    Update INDEX.js
    Reder canvas

--- INDEX.js
+++ INDEX.js
@@ -33,29 +33,12 @@
      // var renderer = new THREE.WebGLRenderer();
    //   renderer.setSize(window.innerWidth, window.innerHeight);
  //     document.body.appendChild(renderer.domElement);
- /*
+ 
      var renderer = new THREE.WebGLRenderer();
      renderer.setSize(window.innerWidth, window.innerHeight);
      document.body.appendChild(renderer.domElement);
- */
-    const canvas = document.getElementById('c');
-    const renderer = new THREE.WebGLRenderer({ canvas });
-    
-    function resizeRendererToDisplaySize(renderer) {
-      const canvas = renderer.domElement;
-      const width = canvas.clientWidth;
-      const height = canvas.clientHeight;
-      const needResize = canvas.width != width || canvas.height !== height;
-      if (needResize) {
-        renderer.setSize(width, height, false);
-      }
-      return needResize;
-    }
-     if (resizeRendererToDisplaySize(renderer)) {
-       const canvas = renderer.domElement;
-       camera.aspect = canvas.clientWidth / canvas.clientHeight ;
-       camera.updateProjectionMatrix();
-     }
+ 
+
     
      
     {
